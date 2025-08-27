1b72626f3e00bb90fd2c4655507e021595eb3741
Stasa Bugarski <stasabugarski@Mac.lan>
2025-08-27 14:53:40 +1200

    fix for the 404

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,23 @@ import Home from './pages/Home';
 import Portfolio from './pages/portfolio/Portfolio';
 import Contact from './pages/Contact';
 import PortfolioVLM from './pages/portfolio/PortfolioVLM';
+import { useLocation } from 'react-router-dom';
+
+
+// Add this hook near the top of your App component
+function useQueryRedirectFix() {
+  const location = useLocation();
+
+  // If app is loaded with query param instead of proper path (after redirect from 404.html)
+  if (location.search && location.pathname === "/") {
+    const realPath = location.search.slice(1);
+    window.history.replaceState(null, "", realPath);
+  }
+}
 
 const App: React.FC = () => {
+  useQueryRedirectFix();
+
   return (
     <Router>
       <Navigation />
